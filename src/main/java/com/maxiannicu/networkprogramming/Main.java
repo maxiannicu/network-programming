4dd69111f31298d7a81bd1cf807add04d9e949a1
maxiannicu <maxiannicu@gmail.com>
2017-03-18 12:27:24 +0200

    Changed to Java8 level. Added print of arguments

--- src/main/java/com/maxiannicu/networkprogramming/Main.java
+++ src/main/java/com/maxiannicu/networkprogramming/Main.java
@@ -1,7 +1,10 @@
 package com.maxiannicu.networkprogramming;
 
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.printf("Arguments count %d\n",args.length);
+        Stream.of(args).forEach(System.out::println);
     }
 }
